809794913b979d9f6b53a4f967da8e4c490226b9
Wall-E <yychan@bu.edu>
2018-12-11 23:21:05 -0500

    "Potential Field done (need to fix local minimum"

---
+++ src/planner/src/manager_v2.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+import rospy
+import time
+import math
+from std_msgs.msg import String
+from std_msgs.msg import Bool
+from planner.msg import manager_msgs
+from using_markers.msg import obstacle_status
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
+
+global agent_x_now
+global agent_y_now
+global initiationPoint
+global init_flag
+init_flag = 0
+initiationPoint = Point()
+pub_manager = rospy.Publisher('/manager_task',manager_msgs, queue_size=10)
+positionNow = rospy.Publisher('/Position',Point,queue_size=10)
+topic = 'agent'
+publisher = rospy.Publisher(topic, Marker,queue_size = 100)
+marker = Marker()
+def makeMarker(x,y):
+        marker.id = 0
+        marker.header.frame_id = "/base"
+        marker.type = marker.SPHERE
+        marker.action = marker.ADD
+        marker.scale.x = 0.5
+        marker.scale.y = 0.5
+        marker.scale.z = 0.2
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.pose.orientation.w = 1.0
+        # Position subscribed from both RRT and Potetial
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = 0
+        publisher.publish(marker)
+
+def dist_calc(x1,y1,x2,y2):
+    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+
+def callback(data):
+    global pt_agent_x
+    global pt_agent_y
+    pt_agent_x = data.x
+    pt_agent_y = data.y
+    makeMarker(pt_agent_x,pt_agent_y)
+    # rospy.loginfo("publishing marker!")
+
+
+def initPoint(start_x,start_y,positionNow):
+    global agent_x_now
+    global agent_y_now
+    global pt_agent_x
+    global pt_agent_y
+    global rrt_agent_x
+    global rrt_agent_y
+    global init_flag
+    agent_x_now = start_x
+    agent_y_now = start_y
+    pt_agent_x = start_x
+    pt_agent_y = start_y
+    rrt_agent_x = start_x
+    rrt_agent_y = start_y
+    rospy.loginfo("Initiating!")
+    global initiationPoint
+    initiationPoint.x = start_x
+    initiationPoint.y = start_y
+    initiationPoint.z = 0
+    makeMarker(start_x,start_y)
+    positionNow.publish(initiationPoint)
+    time.sleep(3)
+    init_flag = 1
+    rospy.loginfo("Done Initiating!")
+
+def selection(data):
+    global agent_x_now
+    global agent_y_now
+    global pt_agent_x
+    global pt_agent_y
+    global rrt_agent_x
+    global rrt_agent_y
+    print("Received!")
+    manager_msg = manager_msgs()
+    # Calculate the distance between the agent and the obstacle
+    # Calcuate the radius
+    # Compare the difference
+    # Send out the flag
+    # radius = data.vx
+    # For now: only pass on pt_coordinates
+    agent_x_now = pt_agent_x
+    agent_y_now = pt_agent_y
+    if init_flag == 1:
+        radius = 2
+        infl_r = 5
+        dist = dist_calc(agent_x_now,agent_y_now,data.x,data.y)
+        if (infl_r>dist):
+            # In the influence circle
+            # True means collision and should run potenrtial field
+            print("Potential!")
+            manager_msg.flag = True
+            manager_msg.infl_dist = infl_r - radius
+            manager_msg.obs_r = radius
+            manager_msg.obs_x = data.x
+            manager_msg.obs_y = data.y
+            manager_msg.agent_x = pt_agent_x
+            manager_msg.agent_y = pt_agent_y
+            pub_manager.publish(manager_msg)
+        else:
+            # Outside circle
+            # False means RRT*
+            print("RRT!")
+            manager_msg.flag = False
+            manager_msg.infl_dist = infl_r - radius
+            manager_msg.obs_r = radius
+            manager_msg.obs_x = data.x
+            manager_msg.obs_y = data.y
+            manager_msg.agent_x = rrt_agent_x
+            manager_msg.agent_y = rrt_agent_y
+            pub_manager.publish(manager_msg)
+    else:
+            pass
+
+
+
+def main():
+    rospy.init_node('manager', anonymous=True)
+    rospy.Subscriber('/PT_position',Point,callback)
+    # initiationPoint = Point()
+    rospy.sleep(0.5)
+    initiation = raw_input("Initiate?:")
+    start_x = float(raw_input("Where do you want to start in the x coordinate:?"))
+    start_y = float(raw_input("y position?:"))
+    yes = 'y'
+    no = 'n'
+    rospy.Subscriber('/planner/obstacle_status',obstacle_status,selection)
+    if initiation == yes:
+        initPoint(start_x,start_y,positionNow)
+    else:
+        pass
+    rospy.spin()
+# Create the agent
+if __name__ == '__main__':
+    main()
--- src/planner/src/potential_field.py
+++ src/planner/src/potential_field.py
@@ -2,7 +2,6 @@
 import rospy
 import math
 import numpy as np
-from numpy import linalg as LA
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 from planner.msg import manager_msgs
@@ -14,7 +13,14 @@ from geometry_msgs.msg import Point
 global counter
 counter = 0
 point_PT = rospy.Publisher('/PT_position',Point,queue_size=100)
-repulsizeWeight = 0.2
+repulsizeWeight = 0.1
+topic = 'pt_arrow'
+publisher = rospy.Publisher(topic, Marker,queue_size = 100)
+marker = Marker()
+ff_points = Point()
+marker.points.append(ff_points)
+marker.points.append(ff_points)
+# marker.points = Point()
 # This node is the potenrial field node where it subscribes to the status of
 # the agent, position, velocity, accelration of the moving obstacle and the static obstacle information
 # to generate the potential field around the obstacles.
@@ -41,36 +47,80 @@ class ReturnValueObstacle(object):
 
 def calculateDistance(x1,y1,x2,y2):
     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    return dist
+    return float(dist)
 
 def staticObs():
-    sobs_1 = obstacle(np.array([10,7]),5,0.2)
-    sobs_2 = obstacle(np.array([-6,6]),6,0.2)
-    sobs_3 = obstacle(np.array([12,5]),6,0.2)
-    sobs_4 = obstacle(np.array([1,1]),4,0.2)
-    return ReturnValueObstacle(sobs_1,sobs_2,sobs_3,sobs_4)
+    obs_1 = obstacle(np.array([1,7]),5,0.1)
+    obs_2 = obstacle(np.array([-9,6]),6,0.1)
+    obs_3 = obstacle(np.array([12,5]),6,0.1)
+    obs_4 = obstacle(np.array([2,-2]),4,0.1)
+    return ReturnValueObstacle(obs_1,obs_2,obs_3,obs_4)
 
 def dist_circle(obstacle,agent_coor):
     dist_agent_to_center = calculateDistance(obstacle.coor[0],obstacle.coor[1],agent_coor[0],agent_coor[1])
     dist_agent_to_surface = dist_agent_to_center - obstacle.r
-    point_sphere = obstacle.r * (1/LA.norm(agent_coor-obstacle.coor))*(agent_coor-obstacle.coor)+obstacle.coor
+    point_sphere = obstacle.r * (1/calculateDistance(agent_coor[0],agent_coor[1],obstacle.coor[0],obstacle.coor[1]))*(agent_coor-obstacle.coor)+obstacle.coor
     return ReturnValue(dist_agent_to_surface,point_sphere)
 
 def U_rep_grad(obstacle,agent_coor):
     dist_sphere = dist_circle(obstacle,agent_coor)
-    grad_U_rep = (-1/dist_sphere.d_s - 1/obstacle.r_i)*(1/(dist_sphere.d_s**2))*((agent_coor-dist_sphere.point_sphere)/(LA.norm(agent_coor-obstacle.coor)))
+    grad_U_rep = -(1/dist_sphere.d_s - 1/obstacle.r_i)*(1/(dist_sphere.d_s**2))*((agent_coor-dist_sphere.point_sphere)/calculateDistance(agent_coor[0],agent_coor[1],obstacle.coor[0],obstacle.coor[1]))
     return grad_U_rep
+# def U_attr_grad(agent_coor):
+#     # Use 'quadratic' shape for attractive
+#     goal = nb.array([-6,15])
+#     U_grad = 2*(1/LA.norm(agent_coor-goal.coor))**
 
 def xPath(obstacle,agent_coor,step):
     static_obstacles = staticObs()
-    total_U_rep_grad = repulsizeWeight * (U_rep_grad(obstacle,agent_coor)+U_rep_grad(static_obstacles.obs1,agent_coor)+\
+    total_U_rep_grad = repulsizeWeight * ((U_rep_grad(obstacle,agent_coor)+U_rep_grad(static_obstacles.obs1,agent_coor)+\
                         U_rep_grad(static_obstacles.obs2,agent_coor)+U_rep_grad(static_obstacles.obs3,agent_coor)+ \
-                        U_rep_grad(static_obstacles.obs4,agent_coor))
+                        U_rep_grad(static_obstacles.obs4,agent_coor)))
+    nrm_grad = calculateDistance(0,0,total_U_rep_grad[0],total_U_rep_grad[1])
+    if nrm_grad > 5:
+        print(total_U_rep_grad)
+        total_U_rep_grad = total_U_rep_grad/nrm_grad
+        print(total_U_rep_grad)
+        print(nrm_grad)
+    else:
+        pass
+    makeArrow(total_U_rep_grad,agent_coor)
     xPath = agent_coor - total_U_rep_grad*step
+
     return xPath
 
 # def callback(static_obs):
 #     obstacle_1 = obstacle(marker.coor,marker.r,marker.r_i)
+def makeArrow(vector,agent_coor):
+        marker.points.pop(0)
+        marker.points.pop(0)
+        marker.id = 11
+        marker.header.frame_id = "/base"
+        marker.type = marker.ARROW
+        marker.action = marker.ADD
+        start_point = Point()
+        start_point.x = 0
+        start_point.y = 0
+        start_point.z = 0.0
+        marker.points.append(start_point)
+        end_point = Point()
+        end_point.x = vector[0] * -10
+        end_point.y = vector[1] * -10
+        end_point.z = 0.0
+        marker.points.append(end_point)
+        marker.scale.x = 0.5
+        marker.scale.y = 0.5
+        marker.scale.z = 0.2
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 1.0
+        marker.color.b = 1.0
+        marker.pose.orientation.w = 1.0
+        # Position subscribed from both RRT and Potetial
+        marker.pose.position.x = agent_coor[0]
+        marker.pose.position.y = agent_coor[1]
+        marker.pose.position.z = 0
+        publisher.publish(marker)
 
 def callback(data):
     agent_coor = np.array([data.agent_x,data.agent_y])
@@ -78,33 +128,24 @@ def callback(data):
     # Add all the static obstacles
     nextPoint_PT = Point()
     if (data.flag == True):
-        nextPoint = xPath(moving_obstacle,agent_coor,0.01)
+        nextPoint = xPath(moving_obstacle,agent_coor,0.05)
         # global counter
         # counter = counter+1
         nextPoint_PT.x = nextPoint[0]
         nextPoint_PT.y = nextPoint[1]
         nextPoint_PT.z = 0
         point_PT.publish(nextPoint_PT)
-        print('Running Potential field')
-        print(nextPoint)
+        # print('Running Potential field')
+        # print(nextPoint)
     else:
-        print('Waiting for collision')
+        # print('Waiting for collision')
         # creates next coordinate until potential field is no longer active
-        
-def callback_initiate(data):
-    firstPoint = Point()
-    firstPoint.x = data.agent_x
-    firstPoint.y = data.agent_y
-    firstPoint.z = 0
-    point_PT.publish(firstPoint)
-    print('Inititating')
-    print(firstPoint)
-
+        pass
 if __name__ == '__main__':
     try:
         rospy.init_node('potential_field', anonymous=True)
         moving_obs = rospy.Subscriber('manager_task',manager_msgs,callback)
-        initiation = rospy.Subscriber('/initiation',manager_msgs,callback_initiate)
+        # initiation = rospy.Subscriber('/initiation',manager_msgs,callback_initiate)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
